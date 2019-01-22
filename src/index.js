783de27f57bdd1184a5aef4ab29a74080cfabede
Jamila Toaha <jtoahacoding@gmail.com>
2019-01-22 16:23:14 -0500

    latest commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('Hello world!');
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoianRvYWhhIiwiYSI6ImNqcjg5NDQwNzA0aTAzenNiNzNwMnFkdGYifQ.kzebsopJbwXSg2YKGfJTrw';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
